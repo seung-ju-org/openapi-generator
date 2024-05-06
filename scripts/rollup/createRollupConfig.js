d04491540fa3f7c815494168816239fca6028f55
Seung Ju <seung-ju@seung-ju.com>
2024-05-06 20:40:08 +0900

    initial commit

---
+++ scripts/rollup/createRollupConfig.js
@@ -0,0 +1,42 @@
+import commonjs from '@rollup/plugin-commonjs';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+export function createRollupConfig(options, callback) {
+  const name = options.name;
+  // A file with the extension ".mjs" will always be treated as ESM, even when pkg.type is "commonjs" (the default)
+  // https://nodejs.org/docs/latest/api/packages.html#packages_determining_module_system
+  const extName = options.format === 'esm' ? 'mjs' : 'js';
+  const outputName = 'dist/bin/' + [name, options.format, extName].join('.');
+
+  const config = {
+    input: options.input,
+    output: {
+      file: outputName,
+      format: options.format,
+      name: '@seung-ju/openapi-generator',
+      sourcemap: true,
+      exports: 'named',
+      banner: '#!/usr/bin/env node',
+    },
+    plugins: [
+      typescript({
+        tsconfig: options.tsconfig,
+        clean: true,
+        exclude: ['**/__tests__', '**/*.test.ts'],
+      }),
+      options.format === 'umd' &&
+        commonjs({
+          include: /\/node_modules\//,
+        }),
+      sourcemaps(),
+      options.format !== 'esm' &&
+        terser({
+          output: { comments: false },
+        }),
+    ].filter(Boolean),
+  };
+
+  return callback ? callback(config) : config;
+}
---
+++ src/openapi-generator.ts
@@ -0,0 +1,4 @@
+import process from 'node:process';
+
+console.log('??');
+console.log(process.argv);
